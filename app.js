ca7a2164f03efb2b89d7d56c772dd82e65197743
Suman Srinivas <sumansrinivas@Sumans-MacBook-Pro.local>
2025-06-27 17:15:38 +0530

    client side form validation performed

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Listing = require('./models/listing.js');
+const path = require('path');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync.js');
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,'/public')));
+
+const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+main().then(()=>{
+    console.log('Successfully connected to DB');
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get('/',(req,res)=>{
+    res.send('root is working');
+});
+
+// app.get('/testListing',async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title : 'Luxurious Double Villa',
+//         description: 'Near by coast',
+//         price : 2000,
+//         location : 'kottayam, kerala',
+//         country : 'India'
+//     });
+
+//     await sampleListing.save();
+//     console.log('sample was saved');
+//     res.send('successful testing');
+// });
+
+//Index Route
+app.get('/listings',async(req,res)=>{
+    const allListings = await Listing.find({}) ;
+    res.render('listings/index.ejs',{allListings});
+});
+
+//NEW Route
+app.get('/listings/new', (req,res)=>{
+    res.render('listings/new.ejs');
+});
+
+//Create Route
+app.post('/listings', wrapAsync(async(req,res,next)=>{
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect('/listings');
+}));
+
+//Show Route
+app.get('/listings/:id',async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(`${id}`);
+    res.render('listings/show.ejs', {listing});
+});
+
+//Edit Route
+app.get('/listings/:id/edit',async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(`${id}`);
+    res.render('listings/edit.ejs', {listing});
+});
+
+//Update Route
+app.put('/listings/:id',async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//Destroy Route
+app.delete('/listings/:id',async(req,res)=>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect('/listings');
+});
+
+app.use((err,req,res,next)=>{
+    res.send('Something went wrong');
+});
+
+app.listen(8080,()=>{
+    console.log('App is listening to port: 8080');
+});
